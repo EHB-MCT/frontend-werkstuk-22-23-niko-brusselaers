f9577eadc3fd4ab250523ca03eb3742b10cf7f0a
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-04-25 00:00:47 +0200

    initial commit

---
+++ wikiwall/src/Root.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Root() {
+  return (
+    <div className="App">
+      <h1>WikiWall</h1>
+    </div>
+  );
+}
+
+export default Root;
---
+++ wikiwall/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter,RouterProvider
+} from "react-router-dom";
+import './index.css';
+import Root from './Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
+
